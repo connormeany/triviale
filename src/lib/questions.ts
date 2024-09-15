ab43112ec2e1c291abc0309c1d7102d63f9aa0d7
Connor Meany <cmeany@stanford.edu>
2024-09-14 20:57:43 -0400

    udpated questions

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -27,37 +27,37 @@ const questions: Question[] = [
     {
         question: "Who wrote Romeo and Juliet?",
         difficulty: 0,
-        date: "2024-09-14",
+        date: "2024-09-15",
         answers: ["William Shakespeare", "Shakespeare"],
     },
     {
         question: "What is the chemical symbol for gold?",
         difficulty: 1,
-        date: "2024-09-14",
+        date: "2024-09-15",
         answers: ["Au"],
     },
     {
         question: "What is the capital of Australia?",
         difficulty: 2,
-        date: "2024-09-14",
+        date: "2024-09-15",
         answers: ["Canberra"],
     },
     {
         question: "Name any one of the three most abundant elements in the earth's crust (making up roughly 47%, 28%, and 8% by weight, respectively).",
         difficulty: 0,
-        date: "2024-09-15",
+        date: "2024-09-14",
         answers: ["Oxygen, Silicon, and Aluminum", "Oxygen", "Silicon", "Aluminum"],
     },
     {
         question: "In December of 2010, an arrest warrant was issued against WikiLeaks founder Julian Assange to extradite him to what country, for questioning on charges related to alleged sex offenses?",
         difficulty: 1,
-        date: "2024-09-15",
+        date: "2024-09-14",
         answers: ["Sweden"],
     },
     {
         question: "The lost and hidden (and fictional) Incan city of Parapata is central to the plot of a 2019 film that features the first live-action film appearance of what title character?",
         difficulty: 2,
-        date: "2024-09-15",
+        date: "2024-09-14",
         answers: ["Dora", "Dora the Explorer"],
     },
 ];
